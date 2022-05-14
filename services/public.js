806559f663729d548055850197f421d7d7a8c83b
Pavel Sheremet <pashqa.sheremet@gmail.com>
2022-05-14 18:29:31 +0300

    clear code

--- services/public.js
+++ services/public.js
@@ -1,14 +1,8 @@
 const path = require('path');
-const { BadRequest } = require('http-errors');
 const { getDirPath } = require('../config');
 const { fsOperations } = require('../helpers');
 
 const uploadAvatar = async (file, endpoint) => {
-  if (!file)
-    throw BadRequest(
-      'Error file format. Supported types: ".jpeg", ".jpg", ".png"',
-    );
-
   const { filename, path: oldPath } = file;
 
   const newPath = path.resolve(getDirPath().avatars, filename);
